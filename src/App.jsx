938aab7f23e6d7cb9deea0d243ad5f43777d98e2
Jacob Hawkins <jacobhawkins7@gmail.com>
2023-01-26 08:42:59 -0800

    added buttons and headers

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import './css/App.css';
 
 function App() {
-  return <p>Cleanedup React App</p>;
+  return (
+    <div className='App'>
+      <h1>Please Contribute To Support Us!</h1>
+      <button>Contribution</button>
+      <h1>
+        Please Click on the following to read up on our Newsletters past and
+        present!
+      </h1>
+      <button>Newsletters</button>
+    </div>
+  );
 }
 
 export default App;
